364fd36e6b941c8165ddeed8712a857dae292b1e
Kevin Mendez <kevnm24@gmail.com>
2018-10-13 13:39:44 -0700

    completed exceeds

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const data = require('./data');
+const data = require('./data.json');
 const app = express();
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
+app.use('/images', express.static('images'));
 
 app.get('/', (req, res) => {
-  const locals = data.projects;
-  res.render('index', {locals})
+  res.render('index', {data})
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/project:id', (req, res) => {
+app.get('/projects:id', (req, res) => {
   res.render('project', {
     data: data,
     id: req.params.id
